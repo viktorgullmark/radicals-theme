0fd32bcdc89bb6b8b9739a3badf42131a5543a3e
Viktor Gullmark <viktor.gullmark@exor.se>
2018-02-17 18:33:50 +0100

    Fixed styles for footer

--- footer.php
+++ footer.php
@@ -8,19 +8,19 @@ global $post;
     <div class="container footer-content">
         <div class="row">
             <div class="col-12 row align-items-center">
-                <div class="col-xs-12 col-sm-5 col-md-6 col-lg-6  align-items-center left-section">
+                <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6  align-items-center left-section">
                     <div class="row">
                         <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
                             <h4>Connect:</h4>
                         </div>
-                        <div class="col-xs-12 col-sm-12">
+                        <div class="col-xs-12 col-sm-12 col-md-8 col-lg-8">
                             <div class="social-media-icons">
                                 <?php get_template_part('partials/social-media-icons'); ?>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div class="col-xs-12 col-sm-7 col-md-6 col-lg-6 right-section">
+                <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 right-section">
                     <span class="copyright-text">
                         <?php the_field('copyright_text', $frontpage) ?>
                     </span>
